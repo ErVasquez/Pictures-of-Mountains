418c12e032e6d063e29b3a5f203a92014c35eb36
Ernie Vasquez <51201085+ErVasquez@users.noreply.github.com>
2019-05-31 09:46:00 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,20 @@
+function image(link) {
+    $(".mountain-picture").attr("src", link);
+}
+
+
+$(".everest").click(function() {
+    image("https://user-images.githubusercontent.com/14214/42292528-246b57fe-7fa1-11e8-83fb-3a0daa76d08e.png");
+});
+
+$(".k2").click(function() {
+     image("https://user-images.githubusercontent.com/14214/42292614-ba367638-7fa1-11e8-8e94-97bc8b566232.png");
+});
+
+$(".kangchenjunga").click(function() {
+     image("https://user-images.githubusercontent.com/14214/42292634-e0fedbac-7fa1-11e8-9d56-6d12dcd7ab3c.png");
+});
+
+$("").click(function() {
+    image("https://www.adventureconsultants.com/assets/Uploads/Himalaya/Nepal-Himalaya/Lhotse/Everest-from-Lhotse.jpg");
+});
